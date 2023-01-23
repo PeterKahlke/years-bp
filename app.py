8e2b2798a59111bb11de3ac3450892a3206f7100
PeterKahlke <izzly@hotmail.com>
2023-01-23 11:43:01 +0100

    Updated text in app.py

--- app.py
+++ app.py
@@ -2,26 +2,26 @@ from datetime import datetime
 
 now = datetime.today()
 
-def YearsBP(years):
+def YearsBP():
+    years = int(input("Enter years. "))
     print(str(years) + " = " + str(1950 - years) + " BP")
     
-def YearsBPtoYearsAD(years):
+def YearsBPtoYearsAD():
+    years = int(input("Enter years. "))
     if years <= 1950:
         print(str(years) + " BP = " + str(1950 - years) + " AD")
     else:
         print(str(years) + " BP = " + str(years - 1950) + " BC")
     
-def YearsBPtoYearsBeforeNow(years):
+def YearsBPtoYearsBeforeNow():
+    years = int(input("Enter years. "))
     print(str(years) + " BP = " + str(years + (now.year - 1950)) + " yrs before now")
     
 def SelectConverter():
-    method = input("1. AD/BC to BP? 2. BP to AD/BC?")
-    print(method)
-
-YearsBP(1650)
-YearsBP(-50)
-YearsBPtoYearsAD(500)
-YearsBPtoYearsAD(1950)
-YearsBPtoYearsAD(1951)
-YearsBPtoYearsBeforeNow(1000)
+    method = input("1. AD/BC to BP? 2. BP to AD/BC? ")
+    if method == "1":
+        YearsBP()
+    if method == "2":
+        YearsBPtoYearsAD()
+        
 SelectConverter()
